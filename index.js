d78a74fbfdd395e324c5f4c66775e27c85ff7dc6
qodvilla <villa@qodbrains.com>
2023-02-14 22:09:43 +0200

    Last attempt for the night

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function onScanSuccess(decodedText, decodedResult) {
   function onScanFailure(error) {
     // handle scan failure, usually better to ignore and keep scanning.
     // for example:
-    alert(`Code scan error = ${error}`);
+    // alert(`Code scan error = ${error}`);
   }
   
   let html5QrcodeScanner = new Html5QrcodeScanner(
     "reader",
     { fps: 10, qrbox: {width: 250, height: 250} },
-    /* verbose= */ false);
+    /* verbose= */ true);
   html5QrcodeScanner.render(onScanSuccess, onScanFailure);
